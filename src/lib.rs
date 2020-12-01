a88d6b188dd69fcad53dcbcc4fc4cdd99a535366
Xiwen <funfoolsuzi@gmail.com>
2020-11-30 22:41:57 -0800

    add quick lib wrapper: default, with_capacity

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,11 @@ mod byte;
 
 pub use byte::Byte;
 pub use buffer::Buffer;
+
+pub fn default() -> Buffer {
+    Buffer::default()
+}
+
+pub fn with_capacity(cap: usize) -> Buffer {
+    Buffer::with_capacity(cap)
+}
