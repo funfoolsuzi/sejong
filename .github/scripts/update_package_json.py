9ebcef92f2a66a662627d751432dfa0e8c3e376a
Xiwen <funfoolsuzi@gmail.com>
2020-12-27 20:16:30 -0800

    add python script to update package.json

---
+++ .github/scripts/update_package_json.py
@@ -0,0 +1,22 @@
+import json, sys
+
+if len(sys.argv) < 2:
+    print('update_package_json.py missing args')
+    sys.exit(1)
+
+file_location = sys.argv[1]
+
+json_str = ''
+with open(file_location, 'r') as f:
+    json_str = ''
+    for line in f.readlines():
+        json_str += line
+ 
+pkg = json.loads(json_str)
+pkg['name'] = 'sejong-buffer'
+if 'files' in pkg:
+    del pkg['files']
+new_json_str = json.dumps(pkg)
+
+with open(file_location, 'w') as f:
+    f.write(new_json_str)
